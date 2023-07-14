740d86112f68c9cff179d7cc5113071ace640248
JaCraig <craig.jamesa@gmail.com>
2023-07-14 15:17:37 -0400

    fix: Adding automation to the project

--- ExampleApp/ExampleCommand1.cs
+++ ExampleApp/ExampleCommand1.cs
@@ -8,21 +8,43 @@ using System.Threading.Tasks;
 
 namespace ExampleApp
 {
-    public enum ExampleEnum
-    {
-        Value1,
-        Value2,
-        Value3
-    }
-
+    /// <summary>
+    /// Example command available to the user to run from the command line
+    /// </summary>
+    /// <seealso cref="CommandBaseClass&lt;ExampleInput&gt;" />
     public class ExampleCommand1 : CommandBaseClass<ExampleInput>
     {
+        /// <summary>
+        /// Gets the aliases. These are the names that can be used to call the command.
+        /// </summary>
+        /// <value>
+        /// The aliases.
+        /// </value>
         public override string[] Aliases { get; } = new[] { "Example1" };
 
+        /// <summary>
+        /// Gets the description. This is what is displayed when the user calls the help command.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
         public override string Description { get; } = "Example command.";
 
+        /// <summary>
+        /// Gets the name. This is the name of the command.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public override string Name { get; } = "Example Command 1";
 
+        /// <summary>
+        /// Runs the specified input. This is where the command does its work.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>
+        /// The result.
+        /// </returns>
         protected override Task<int> Run(ExampleInput input)
         {
             Console.WriteLine(input.ExampleEnum);
@@ -30,19 +52,64 @@ namespace ExampleApp
         }
     }
 
+    /// <summary>
+    /// Example input for the command.
+    /// </summary>
     public class ExampleInput
     {
+        /// <summary>
+        /// Gets or sets the example enum. This is an example of an enum input.
+        /// </summary>
+        /// <value>
+        /// The example enum.
+        /// </value>
         public ExampleEnum ExampleEnum { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name value. This is an example of a string input. The <see cref="DynamicDisplayAttribute"/> is used to display the current assemblies loaded.
+        /// </summary>
+        /// <value>
+        /// The name value.
+        /// </value>
         [DynamicDisplay(typeof(NameValueDisplay))]
         public string NameValue { get; set; }
     }
 
+    /// <summary>
+    /// Used to display the current assemblies loaded.
+    /// </summary>
     internal class NameValueDisplay
     {
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return "Assemblies currently loaded: " + new ServiceCollection().AddCanisterModules().BuildServiceProvider().GetServices<Assembly>().ToString(x => x.GetName().Name);
         }
+    }
+
+    /// <summary>
+    /// Example enum for the possible values
+    /// </summary>
+    public enum ExampleEnum
+    {
+        /// <summary>
+        /// The value1
+        /// </summary>
+        Value1,
+
+        /// <summary>
+        /// The value2
+        /// </summary>
+        Value2,
+
+        /// <summary>
+        /// The value3
+        /// </summary>
+        Value3
     }
 }
---
+++ docfx_project/articles/intro.md
@@ -0,0 +1,5 @@
+# Program Code
+[!code-csharp[](../../ExampleApp/Program.cs)]
+
+# Command Code
+[!code-csharp[](../../ExampleApp/ExampleCommand1.cs)]
